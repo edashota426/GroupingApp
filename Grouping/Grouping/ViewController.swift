0951fd66af8336f9f30514437281242e6de46eb9
登坂直弥 <naoyan_apple@yahoo.co.jp>
2017-11-26 18:03:09 +0900

    please try and tell me how work.
    please try and tell me how work.

--- Grouping/Grouping/ViewController.swift
+++ Grouping/Grouping/ViewController.swift
@@ -19,7 +19,58 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+//ここから書き足していったコード
+}//icloud containerにあるcsvデータを引数に名前を表示すること
+class ViewController2: UIViewController,UITableViewDataSource {
+    
+    //hsdGroupingTest.csv
+    var dataList:[String] = []
+    
+    
+    //最初からあるメソッド
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do {
+            //CSVファイルのパスを取得する。
+            let csvPath = Bundle.main.path(forResource: "hsdGroupingTest", ofType: "csv")
+            
+            //CSVファイルのデータを取得する。
+            let csvData = try String(contentsOfFile:csvPath!, encoding:String.Encoding.utf8)
+            
+            //改行区切りでデータを分割して配列に格納する。
+            dataList = csvData.components(separatedBy: "\n")
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    
+    //データを返すメソッド
+    func tableView(_ tableView:UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
+        
+        //セルを取得する。
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TestCell", for:indexPath) as UITableViewCell
+        
+        //カンマでデータを分割して配列に格納する。
+        let dataDetail = dataList[indexPath.row].components(separatedBy: ",")
+        
+        //セルのラベルにsex,skill,Region,Presen,JFを設定する。
+        cell.textLabel?.text = dataDetail[5]
+        cell.detailTextLabel?.text = "Sex：" + String(dataDetail[1])
+        
+        return cell
+    }
+    
+    
+    //データの個数を返すメソッド
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        return dataList.count
+    }
 }
+//
+
+
+
 
